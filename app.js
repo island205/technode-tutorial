96779e18ddd20972551dcc88387f9bba281b2b03
island205 <island205@gmail.com>
2013-10-27 21:22:35 +0800

    add socketio more style

--- app.js
+++ app.js
@@ -2,8 +2,35 @@ var express = require('express')
 var app = express();
 var port = process.env.PORT || 3000
 
+var rooms = {
+  'javascript': {
+    name: 'javascript',
+    users: []
+  },
+  'node.js': {
+    name: 'node.js',
+    users: []
+  }
+}
+
 app.use(express.static(__dirname + '/webapp'))
 
 var io = require('socket.io').listen(app.listen(port))
+
+io.sockets.on('connection', function (socket) {
+  socket.on('read:rooms', function () {
+    socket.emit('read:rooms', Object.keys(rooms).map(function (roomName) {
+      return {name: roomName}
+    }))
+  })
+
+  socket.on('add:user', function (user) {
+    rooms[user.room].users.push({
+      email: user.email,
+      socket:socket
+    })
+    socket.broadcast.emit('add:user', user)
+  })
+})
 
 console.log("nodechat  is on port " + port + '!')
--- webapp/src/nodechat.js
+++ webapp/src/nodechat.js
@@ -1,3 +1,42 @@
 $(function () {
-  //$('.log-in-modal').modal()
+  $('.log-in-modal').modal()
+  var socket = io.connect('http://localhost:3000')
+
+  socket.on('read:rooms', function (rooms) {
+    // modal for login name
+    $.tmpl('<li><a href="#">${name}</a></li>', rooms).appendTo($('.dropdown-rooms'))
+    var $dropdownRoomsLabel = $('.dropdown-rooms-label').html(rooms[0].name)
+
+    // render rooms
+    $.tmpl('<a href="#" class="list-group-item">${name}</a>', rooms)
+      .appendTo($('.room-list'))
+    $('.room-list a').first().addClass('active')
+    $('.room-header').html(rooms[0].name)
+
+    $('.log-in-modal').modal()
+    $('.dropdown-rooms li a').on('click', function () {
+      $dropdownRoomsLabel.html($(this).html())
+    })
+
+    var $userEmail = $('.user-email')
+
+    $('.log-in-modal .btn-enter').on('click', function (evt) {
+      evt.preventDefault()
+      if (/^.*@gmail.com$/.test($userEmail.val())) {
+        socket.emit('add:user', {
+          email: $userEmail.val(),
+          room: $dropdownRoomsLabel.html()
+        })
+        $('.log-in-modal').modal('hide')
+      } else {
+        $userEmail.focus()
+      }
+    })
+  })
+
+  socket.on('add:user', function (user) {
+    console.log(user)
+  })
+
+  socket.emit('read:rooms')
 })
