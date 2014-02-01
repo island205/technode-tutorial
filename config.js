dd5f2381f8675675f03afc449da725bcce5f28af
island205 <island205@gmail.com>
2014-02-01 12:24:15 +0800

    done

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
+	//   mongodb: 'mongodb://localhost/technodechapter02',
   mongodb: 'mongodb://technode:technode@troup.mongohq.com:10046/technode',
   robot: {
     name: 'technode机器人',
