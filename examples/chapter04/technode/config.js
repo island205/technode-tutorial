d91b13bbd69661612406cb0c7907fd5e5689037b
island205 <island205@gmail.com>
2014-08-22 23:51:23 +0800

    i don't known

--- examples/chapter04/technode/config.js
+++ examples/chapter04/technode/config.js
@@ -1,6 +1,5 @@
 module.exports = {
-	//   mongodb: 'mongodb://localhost/technodechapter02',
-  mongodb: 'mongodb://technode:technode@troup.mongohq.com:10046/technode',
+	mongodb: 'mongodb://localhost/technodechapter04',
   robot: {
     name: 'technode机器人',
     avatarUrl: '/images/technode.png'
