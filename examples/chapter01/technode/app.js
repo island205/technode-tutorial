00bc096940f1f174a9197153872e304d792ae412
island205 <island205@gmail.com>
2014-12-06 12:22:39 +0800

    update chapter01 example

--- examples/chapter01/technode/app.js
+++ examples/chapter01/technode/app.js
@@ -1,26 +1,28 @@
 var express = require('express')
 var app = express()
+var path = require('path')
 var port = process.env.PORT || 3000
 
-app.use(express.static(__dirname + '/static'))
+app.use(express.static(path.join(__dirname, '/static')))
 
-app.use(function (req, res) {
+app.use(function(req, res) {
   res.sendfile('./static/index.html')
 })
 
-var io = require('socket.io').listen(app.listen(port))
+server = app.listen(port, function() {
+  console.log('TechNode  is on port ' + port + '!')
+})
 
+var io = require('socket.io').listen(server)
 
 var messages = []
 
-io.sockets.on('connection', function (socket) {
-  socket.on('messages.read', function () {
+io.sockets.on('connection', function(socket) {
+  socket.on('messages.read', function() {
     socket.emit('messages.read', messages)
   })
-  socket.on('messages.create', function (message) {
+  socket.on('messages.create', function(message) {
     messages.push(message)
     io.sockets.emit('messages.add', message)
   })
 })
-
-console.log('TechNode  is on port ' + port + '!')
