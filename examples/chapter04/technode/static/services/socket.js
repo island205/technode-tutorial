e4e22e98cbd41e4f80e6b47bb9a676ce30b7c88b
yuanzm <1229084233@qq.com>
2015-06-04 11:26:20 +0800

    fix socket connect bug in all chapters

--- examples/chapter04/technode/static/services/socket.js
+++ examples/chapter04/technode/static/services/socket.js
@@ -1,5 +1,9 @@
 angular.module('techNodeApp').factory('socket', ['$rootScope', function($rootScope) {
-  var socket = io.connect('/')
+  // var socket = io.connect('/')
+  //为了避免跨域请求，需要将原来的var socket = io.connect('/')改成下面这一行
+  // var socket = io.connect('http://localhost:3000/')
+  //或者采用socket官方主页上面的方法 var socket = io()
+  var socket = io();
   return {
     on: function(eventName, callback) {
       socket.on(eventName, function() {
